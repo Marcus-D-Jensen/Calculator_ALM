ddd0a25fb753c42972a39c0c2aecbef5a27237cd
William Jelvin <william.jelvin@gmail.com>
2019-08-22 14:56:39 +0200

    added a check for division by zero

--- src/calculator_alm/Calculate.java
+++ src/calculator_alm/Calculate.java
@@ -3,6 +3,9 @@ package calculator_alm;
 public class Calculate {
     
     public double divide(double num1, double num2) {
-        return num1/num2;
+        if (num2 != 0) {
+            return num1/num2;
+        }
+        return 0;
     }    
 }
